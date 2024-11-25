566571467555cb303895744c9fcb4a6a85245af5
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-11-25 16:14:50 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,6 +3,7 @@ import os
 API_ID    = os.environ.get("API_ID", "")
 API_HASH  = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
+OWNER_ID = os.environ.get("OWNER_ID", "")
+SUDO_USERS = os.environ.get("SUDO_USERS", "")
 
 PORT = 8080
